9eaa49a0d440f7ecc1eae5c85b84f2fabd1b548e
taikio <elkerafereira@hotmail.com>
2025-09-28 17:54:26 -0300

    chore: seta compatibilityDate no nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,19 @@ import generateMeta from './utils/generateMeta'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content'],
+
   nitro: {
     prerender: {
       routes: ['/sitemap.xml'],
     },
   },
+
   content: {
     highlight: {
       theme: 'dracula',
     },
   },
+
   app: {
     head: {
       title: 'Blog Papo Digital',
@@ -19,4 +22,6 @@ export default defineNuxtConfig({
       link: [{ rel: 'canonical', href: 'https://www.papodigital.net.br' }],
     },
   },
+
+  compatibilityDate: '2025-09-28',
 })
